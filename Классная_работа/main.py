4368888a5c63612f61e85dd5e4e36809ba43a9f8
Chuhanchik <medegor021180@gmail.com>
2025-04-07 17:06:16 +0300

    Add files via upload

--- Классная_работа/main.py
+++ Классная_работа/main.py
@@ -22,5 +22,36 @@ def main():
     app.run()
 
 
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = "Karim"
+    user.name = "Islyamov"
+    user.age = "12"
+    user.position = "Programmist"
+    user.speciality = "Bezdelnik"
+    user.address = "Kirpichka_231"
+    user.email = "BigKarim@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run()
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = User()
+    user.surname = "Matvei"
+    user.name = "Molenov"
+    user.age = "16"
+    user.position = "MCtraher"
+    user.speciality = "SYSAdmin"
+    user.address = "pppp_1"
+    user.email = "qwerty123123@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run()
+
 if __name__ == '__main__':
     main()
